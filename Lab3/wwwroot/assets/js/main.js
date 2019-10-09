a15a65db1973529c5cbaf094aef91fc7c1dc720f
Duc Tran <tuf49524@temple.edu>
2019-10-09 10:33:48 -0400

    changes to js and table

--- Lab3/wwwroot/assets/js/main.js
+++ Lab3/wwwroot/assets/js/main.js
@@ -1,1 +1,8 @@
-﻿
+﻿var itemType, itemName, itemDesc, itemPrice;
+
+function item(type, name, desc, price) {
+    this.itemType = type;
+    this.itemName = name;
+    this.itemDesc = desc;
+    this.itemPrice = price;
+}
